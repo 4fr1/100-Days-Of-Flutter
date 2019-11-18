be604afa9c72e560894427707dc4eca8d0dc5e87
Lukas Schneider <kontakt@lukasschneider.info>
2019-11-18 12:36:32 +0100

    Day 36

---
+++ day36_form_validation/lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Center(child: CustomForm()),
+    ));
+  }
+}
+
+class CustomForm extends StatefulWidget {
+  @override
+  _CustomFormState createState() => _CustomFormState();
+}
+
+class _CustomFormState extends State<CustomForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            }),
+
+            RaisedButton(
+                onPressed: () {
+                  if (_formKey.currentState.validate()) {
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text("Success!!")));
+                  }
+                },
+                child: Text("Check"))
+          ],
+        ));
+  }
+}
